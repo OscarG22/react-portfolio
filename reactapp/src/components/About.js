5600862cc3c968f78c0c01bbff0fe10553c6fd4a
Oscar Grajeda <oscargrajeda2@gmail.com>
2021-01-20 20:44:47 -0700

    added 'about me' file

---
+++ reactapp/src/components/About.js
@@ -0,0 +1,22 @@
+
+
+
+function About() {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>
+                Hello, I'm Oscar Grajeda. I'm 25 years old, and I'm the oldest of three. I graduated from Dobson
+                High School in 2014, afterwards I attended community college for four years and didn't find anything to major in. It was very difficult for me to find something I enjoyed and/or wanted to persue as a career so I gave up college and dedited to work until I found something worth my time and money.
+        </p>
+            <p>
+                Luckily I was introduced to coding in early 2020 by a friend. That led me to doing extensive
+                research and that's how I found Univercity of Arizona's Coding Bootcamp. It has been challenging
+                but also fun since day one (yes I said fun). To my surprise I ended up really liking the world
+                of Web Development.
+        </p>
+        </div>
+    );
+}
+
+export default About;
